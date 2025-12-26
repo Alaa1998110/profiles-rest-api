4f08a1e272ceb46626999b7564cd3dab9da8774c
Alaa1998110 <elsharkawya575@gmail.com>
2025-12-26 19:33:14 +0200

    added searching filters to profileviewset via name and email

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -7,6 +7,7 @@ from rest_framework import viewsets
 from . import models
 from rest_framework.authentication import TokenAuthentication
 from . import permissions
+from rest_framework import filters
 
 class HelloApiView(APIView):
 
@@ -78,3 +79,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset=models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
